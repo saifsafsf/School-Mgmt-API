a269d1cc2753e1dd508002ed3f9b9ebc7480d91b
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2024-02-29 08:31:29 +0500

    pydantic schema added for the db

---
+++ schemas.py
@@ -0,0 +1,85 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class DepartmentBase(BaseModel):
+    name: str
+
+class DepartmentCreate(DepartmentBase):
+    pass
+
+class Department(DepartmentBase):
+    id : int
+
+    class Config:
+        orm_mode = True
+
+
+class StudentBase(BaseModel):
+    email: str
+    dept_id: int
+
+class StudentCreate(StudentBase):
+    pass
+
+class Student(StudentBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TeacherBase(BaseModel):
+    email: str
+    dept_id: int
+
+class TeacherCreate(TeacherBase):
+    pass
+
+class Teacher(TeacherBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SubjectBase(BaseModel):
+    name: str
+    description: str
+
+class SubjectCreate(SubjectBase):
+    pass
+
+class Subject(SubjectBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class EnrollmentBase(BaseModel):
+    student_id: int
+    subject_id: int
+
+class EnrollmentCreate(EnrollmentBase):
+    pass
+
+class Enrollment(EnrollmentBase):
+    pass
+
+    class Config:
+        orm_mode = True
+
+
+class TeachingBase(BaseModel):
+    teacher_id: int
+    subject_id: int
+
+class TeachingCreate(TeachingBase):
+    pass
+
+class Teaching(TeachingBase):
+    pass
+
+    class Config:
+        orm_mode = True
