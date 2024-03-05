b10d3d79d0cd50312907fde76ad819896bbead5a
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2024-03-05 23:32:40 +0500

    fixing imports

--- scripts/database.py
+++ scripts/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from config import settings
+from ..config import settings
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{settings.db_user}:{settings.db_pass}@{settings.db_host}/{settings.db_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
